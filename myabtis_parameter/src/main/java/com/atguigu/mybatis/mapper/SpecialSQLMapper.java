14e2ec817ebc3387eeadc70139fad1d2a7dec125
zhengjian <1634590940@qq.com>
2024-05-26 20:11:50 +0800

    模糊查询

---
+++ myabtis_parameter/src/main/java/com/atguigu/mybatis/mapper/SpecialSQLMapper.java
@@ -0,0 +1,21 @@
+package com.atguigu.mybatis.mapper;
+
+import com.atguigu.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author ZJ
+ * @ClassName SpecialSQLMapper
+ * @Description TODO
+ * @date 2024/5/26 19:44
+ * @Version 1.0
+ */
+public interface SpecialSQLMapper {
+
+    /**
+     * 通过用户名魔术查询
+     */
+    List<User> getUserByFuzzy(@Param("fuzzy") String fuzzy);
+}
---
+++ myabtis_parameter/src/test/java/com/atguigu/mybatis/test/SpecialSQLMapperTest.java
@@ -0,0 +1,28 @@
+package com.atguigu.mybatis.test;
+
+import com.atguigu.mybatis.mapper.SelectMapper;
+import com.atguigu.mybatis.mapper.SpecialSQLMapper;
+import com.atguigu.mybatis.pojo.User;
+import com.atguigu.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ZJ
+ * @ClassName SpecialSQLMapperTest
+ * @Description TODO
+ * @date 2024/5/26 19:54
+ * @Version 1.0
+ */
+public class SpecialSQLMapperTest {
+    @Test
+    public void getAllUserToMap(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        SpecialSQLMapper mapper = sqlSession.getMapper(SpecialSQLMapper.class);
+        List<User> list = mapper.getUserByFuzzy("a");
+        list.forEach(System.out::println);
+    }
+}
