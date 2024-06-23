4da9f726390760cf9ca3354db8953cce4b6d2c3e
zhengjian <1634590940@qq.com>
2024-06-23 19:32:08 +0800

    关键字之foreach(批量增加，批量删除)

--- mybatis_dynamicSQL/src/main/java/com/atguigu/mybatis/mapper/DynamicSQLMapper.java
+++ mybatis_dynamicSQL/src/main/java/com/atguigu/mybatis/mapper/DynamicSQLMapper.java
@@ -1,6 +1,7 @@
 package com.atguigu.mybatis.mapper;
 
 import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -26,4 +27,16 @@ public interface DynamicSQLMapper {
      * @return
      */
     List<Emp> getEmpByChoose(Emp emp);
+
+    /**
+     * 批量添加员工信息
+     * @param emps
+     */
+    void insertMoreEmp(@Param("emps") List<Emp> emps);
+
+    /**
+     * 批量删除
+     * @param empIds
+     */
+    void deleteMoreEmp(@Param("empIds")Integer[] empIds);
 }
--- mybatis_dynamicSQL/src/test/java/com/atguigu/mybatis/test/DynamicSQLMapperTest.java
+++ mybatis_dynamicSQL/src/test/java/com/atguigu/mybatis/test/DynamicSQLMapperTest.java
@@ -6,6 +6,7 @@ import com.atguigu.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -33,5 +34,24 @@ public class DynamicSQLMapperTest {
         Emp emp = new Emp(null, "zs", 20, "男");
         List<Emp> list = mapper.getEmpByChoose(emp);
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void insertMoreEmp(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Emp emp1 = new Emp(null, "zs1", 20, "男");
+        Emp emp2 = new Emp(null, "zs2", 20, "男");
+        Emp emp3 = new Emp(null, "zs3", 20, "男");
+        List<Emp> emps = Arrays.asList(emp1, emp2, emp3);
+        mapper.insertMoreEmp(emps);
+    }
+
+    @Test
+    public void deleteMoreEmp(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Integer[] empIds = new Integer[]{6,7};
+        mapper.deleteMoreEmp(empIds);
     }
 }
