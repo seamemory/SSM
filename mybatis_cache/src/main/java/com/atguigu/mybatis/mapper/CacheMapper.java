eded990d6ae91cfc14a805d44125db357a4996fa
zhengjian <1634590940@qq.com>
2024-06-25 21:06:19 +0800

    mybatis一级缓存

---
+++ mybatis_cache/src/main/java/com/atguigu/mybatis/mapper/CacheMapper.java
@@ -0,0 +1,21 @@
+package com.atguigu.mybatis.mapper;
+
+import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author ZJ
+ * @ClassName CacheMapper
+ * @Description TODO
+ * @date 2024/6/23 21:43
+ * @Version 1.0
+ */
+public interface CacheMapper {
+
+    /**
+     * 根据员工id查询员工信息
+     * @param empId
+     * @return
+     */
+    Emp getEmpById(@Param("empId") Integer empId);
+}
---
+++ mybatis_cache/src/test/java/com/atguigu/mybatis/CacheMapperTest.java
@@ -0,0 +1,40 @@
+package com.atguigu.mybatis;
+
+import com.atguigu.mybatis.mapper.CacheMapper;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+/**
+ * @author ZJ
+ * @ClassName CacheMapperTest
+ * @Description TODO
+ * @date 2024/6/23 22:08
+ * @Version 1.0
+ */
+public class CacheMapperTest {
+
+    /**
+     * 一级缓存是SqlSession级别的，通过同一个SqlSession查询的数据会被缓存，下次查询相同的数据，就会从缓存中直接获取，不会从数据库重新访问
+     * 使一级缓存失效的四种情况：
+     *  1)不同的SqlSession对应不同的一级缓存
+     *  2)同一个SqlSession但是查询条件不同
+     *  3)同一个SqlSession两次查询期间执行了任何一次增删改操作
+     *  4)同一个SqlSession两次查询期间手动清空了缓存
+     */
+
+    @Test
+    public void getEmpById(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        CacheMapper mapper = sqlSession.getMapper(CacheMapper.class);
+        Emp emp = mapper.getEmpById(1);
+        System.out.println("emp = " + emp);
+        Emp emp1 = mapper.getEmpById(1);
+        System.out.println("emp1 = " + emp1);
+        SqlSession sqlSession2 = SqlSessionUtil.getSqlSession();
+        CacheMapper mapper2 = sqlSession.getMapper(CacheMapper.class);
+        Emp emp2 = mapper.getEmpById(1);
+        System.out.println("emp2 = " + emp2);
+    }
+}
