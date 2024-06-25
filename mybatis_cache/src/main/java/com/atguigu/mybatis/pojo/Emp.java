d6671c96ac5e695dd6dd8fb5693970fbc7e0b903
zhengjian <1634590940@qq.com>
2024-06-25 22:04:07 +0800

    mybatis二级缓存

--- mybatis_cache/src/main/java/com/atguigu/mybatis/pojo/Emp.java
+++ mybatis_cache/src/main/java/com/atguigu/mybatis/pojo/Emp.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 /**
  * @author ZJ
  * @ClassName Emp
@@ -14,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Emp {
+public class Emp implements Serializable {
 
     private Integer empId;
 
--- mybatis_cache/src/test/java/com/atguigu/mybatis/CacheMapperTest.java
+++ mybatis_cache/src/test/java/com/atguigu/mybatis/CacheMapperTest.java
@@ -3,8 +3,14 @@ package com.atguigu.mybatis;
 import com.atguigu.mybatis.mapper.CacheMapper;
 import com.atguigu.mybatis.pojo.Emp;
 import com.atguigu.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author ZJ
@@ -36,5 +42,25 @@ public class CacheMapperTest {
         CacheMapper mapper2 = sqlSession.getMapper(CacheMapper.class);
         Emp emp2 = mapper.getEmpById(1);
         System.out.println("emp2 = " + emp2);
+    }
+
+    /**
+     * 二级缓存测试
+     * @throws IOException
+     */
+    @Test
+    public void testCache() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession1 = sqlSessionFactory.openSession(true);
+        CacheMapper mapper1 = sqlSession1.getMapper(CacheMapper.class);
+        Emp emp1 = mapper1.getEmpById(1);
+        System.out.println("emp1 = " + emp1);
+        sqlSession1.close();
+        SqlSession sqlSession2 = sqlSessionFactory.openSession(true);
+        CacheMapper mapper2 = sqlSession2.getMapper(CacheMapper.class);
+        Emp emp2 = mapper2.getEmpById(1);
+        System.out.println("emp2 = " + emp2);
+        sqlSession2.close();
     }
 }
